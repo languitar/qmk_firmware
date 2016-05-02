10f78a16b3f6f5aab4f91d3d9832a0096fe6e4bf
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2016-05-02 10:46:05 +0200

    Add Home, End, Page nav to nav layer

--- keyboard/ergodox_ez/keymaps/languitar/keymap.c
+++ keyboard/ergodox_ez/keymaps/languitar/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,
+       KC_TRNS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_MUTE,
                  KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_TRNS, KC_MPLY,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP,
                           KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_TRNS,
