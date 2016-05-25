24815e2c4dcced86edc9d6055383558c5b116e15
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2016-05-25 14:40:06 +0200

    Put control as toggle on home row outer buttons

--- keyboards/ergodox_ez/keymaps/languitar/keymap.c
+++ keyboards/ergodox_ez/keymaps/languitar/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   LGUI(KC_SPC),
         KC_TAB,         LT(UMLS,KC_Q),KC_W,   KC_E,   KC_R,   KC_T,   KC_FN1,
-        KC_ESC,         LT(MDIA,KC_A),KC_S,   KC_D,   KC_F,   KC_G,
+        CTL_T(KC_ESC),  LT(MDIA,KC_A),KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        LT(SYMB,KC_Z),KC_X,   KC_C,   KC_V,   KC_B,   KC_EQL,
         KC_LCTRL,       KC_LBRC,      KC_RBRC,KC_LGUI,KC_LALT,
                                                       KC_HOME,KC_END,
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              LGUI(KC_ENT),KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
              LSFT(KC_INS),KC_Y,   KC_U,   KC_I,   KC_O,   LT(UMLS, KC_P),   KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
+                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),CTL_T(KC_QUOT),
              KC_MINS,     KC_N,   KC_M,   KC_COMM,KC_DOT, LT(SYMB,KC_SLSH), KC_RSFT,
                                   KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          KC_RCTRL,
              KC_PGDN,   KC_PGUP,
